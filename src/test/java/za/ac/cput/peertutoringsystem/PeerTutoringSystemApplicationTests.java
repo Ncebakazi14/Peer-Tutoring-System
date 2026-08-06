6ab2a6d02e00381667de702a775a77d9b160d4f2
Ncebakazi14 <231097379@mycput.ac.za>
2026-08-06 17:09:03 +0200

    Initial Spring Boot project setup

---
+++ src/test/java/za/ac/cput/peertutoringsystem/PeerTutoringSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package za.ac.cput.peertutoringsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeerTutoringSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
