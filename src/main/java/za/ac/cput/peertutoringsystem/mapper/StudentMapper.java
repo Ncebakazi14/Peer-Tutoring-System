34354daeb3ac7f3d362004e68bfc0e3ecb8667ec
Moltinate230074189 <230074189@mycput.ac.za>
2026-08-10 15:26:15 +0200

    Add Student mapper

--- src/main/java/za/ac/cput/peertutoringsystem/mapper/StudentMapper.java
+++ src/main/java/za/ac/cput/peertutoringsystem/mapper/StudentMapper.java
@@ -1,4 +1,25 @@
 package za.ac.cput.peertutoringsystem.mapper;
 
+import za.ac.cput.peertutoringsystem.domain.Student;
+import za.ac.cput.peertutoringsystem.dto.StudentResponseDTO;
+
 public class StudentMapper {
-}
+
+        public static StudentResponseDTO toResponseDTO(Student student) {
+
+            if (student == null) {
+                return null;
+            }
+
+            return new StudentResponseDTO(
+                    student.getStudentId(),
+                    student.getFirstName(),
+                    student.getLastName(),
+                    student.getUser().getEmail(),
+                    student.getPhoneNumber(),
+                    student.getDateOfBirth(),
+                    student.isActive()
+            );
+        }
+    }
+
